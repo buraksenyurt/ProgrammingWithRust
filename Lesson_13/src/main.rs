d4a1ca432fc6bd24e2bd8c453fab17790ce3d0fb
buraksenyurt <burakselimsenyurt@gmail.com>
2025-05-18 18:07:42 +0300

    Added proc macro sample

--- Lesson_13/src/main.rs
+++ Lesson_13/src/main.rs
@@ -1,3 +1,16 @@
 mod samples;
 
-fn main() {}
+use procs::work_time_effort;
+
+#[work_time_effort]
+fn find_total() {
+    let mut total = 0;
+    for i in 0..1000 {
+        total += i;
+    }
+    println!("Result: {}", total);
+}
+
+fn main() {
+    let _total = find_total();
+}
